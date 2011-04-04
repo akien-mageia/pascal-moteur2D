b142c75ba4628bb93d6286971a92768d4e78d033
tpourcelot <tpourcelot@6f620d79-b0f1-4916-b3a6-c651a60d58ee>
2011-04-04 08:52:48 +0000

    Ajout de quelques constructeurs

--- trunk/backup/udessinobjet.pas.bak
+++ trunk/backup/udessinobjet.pas.bak
@@ -5,10 +5,16 @@ unit UDessinObjet;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs; 
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  StdCtrls, ExtCtrls;
 
 type
+
+  { TForm3 }
+
   TForm3 = class(TForm)
+   PaintBox1: TPaintBox;
+   procedure FormCreate(Sender: TObject);
   private
     { private declarations }
   public
@@ -19,6 +25,13 @@ var
   Form3: TForm3; 
 
 implementation
+
+{ TForm3 }
+
+procedure TForm3.FormCreate(Sender: TObject);
+begin
+
+end;
 
 initialization
   {$I udessinobjet.lrs}
--- trunk/ufrottement.pas
+++ trunk/ufrottement.pas
@@ -12,6 +12,8 @@ type CFrottement = Class(CTorseur)
 		fCoef: real;	// coefficient de frottement
 	
 	public
+                Constructor Create(aCoef : Real); override;
+                Destructor Destroy ; override;
 		Procedure calculForce(aForme: CForme; aVitesse: CVitesse); override;
 		Procedure calculCoef(aForme: CForme);
 		Procedure setCoef(aCoef: real);
@@ -19,6 +21,17 @@ type CFrottement = Class(CTorseur)
 	end;
 
 implementation
+
+Constructor Cfrottement.Create(aCoef:Real);
+Begin
+     SetCoef(aCoef);
+End;
+ 
+Destructor Destroy; Override;
+
+Begin
+     inherited;
+end;
 
 Procedure CFrottement.calculForce(aForme: CForme; aVitesse: CVitesse);
 Begin
@@ -31,11 +44,14 @@ Begin
 End;
 
 Procedure CFrottement.setCoef(aCoef: real);
+//Permet d'assigner le coeff
 Begin
     fCoef := aCoef;
 End;
 
 Function CFrottement.getCoef(): real;
+//Retourne le coefficient de frottement du solide considere
+
 Begin
     getCoef := fCoef;
 End;
