3b8a0e4f966377ff37d2d8c264c143670076cf0b
tpourcelot <tpourcelot@6f620d79-b0f1-4916-b3a6-c651a60d58ee>
2011-04-11 09:34:54 +0000

    Divers

--- trunk/uposition.pas
+++ trunk/uposition.pas
@@ -15,34 +15,93 @@ Type CPosition = Class
         fYmetre:real;
 
     public
-    	Constructor Create(aXPix,aYPix,aXmtr,aYmtr);virtual;
+    	Constructor Create(aXPix,aYPix : Integer);
     	Destructor  Destroy;override;
+    	
     	//Accessors
-    	Function GetXPixel():Real;
-    	Function GetYPixel():Real;
-    	Function GetXMetre():Real;
-    	Function GetYMetre():Real;
+    	Function GetXPixel() : Integer;
+    	Function GetYPixel() : Integer;
+    	Function GetXMetre() : Real;
+    	Function GetYMetre() : Real;
     	
-    	
+    	Procedure SetXPixel(aXPix : Integer);
+    	Procedure SetYPixel(aYPix : Integer);
+    	Procedure SetXMetre(aXMetre : Real);
+    	Procedure SetYMetre(aYMetre : Real);
     	//Others Functions
-        Procedure pixelToMetre();
-        Procedure metreToPixel();
-    end;
+        Function pixelToMetre(aPos : Integer) : Real;
+        Function metreToPixel(aPos : Real) : Integer;
+end;
 
 implementation
 
-Constructor CPosition.Create(aXPix,aYPix:Real);
+Constructor CPosition.Create(aXPix,aYPix:Integer);
+	//Creation with pixels coordinates
 	Begin;
+		SetXPixel(aXPix);
+		SetYPixel(aYPix);
+		//Conversion between pixels&meters is handled by SetXpixel etc
 	End;
 
-Procedure CPosition.pixelToMetre();
+Destructor CPosition.Destroy;
 	Begin
-
+		Inherited;
 	End;
 
-Procedure CPosition.metreToPixel();
+//Implementation of accessors
+Procedure CPosition.SetXPixel(aXPix : Integer);
 	Begin
+		fXpixel := aXPix;
+		fXmetre := pixelToMetre(aXPix);
+	End;
 
+Procedure CPosition.SetYPixel(aYPix : Integer);
+	Begin
+		fYpixel := aYPix;
+		fYmetre := pixelToMetre(aYPix);
+	End;
+
+Procedure CPosition.SetXMetre(aXmetre : Real);
+	Begin
+		fXmetre := aXmetre;
+		fXPixel	:= metreToPixel(aXMetre);
+	End;
+
+Procedure CPosition.SetYMetre(aYMetre : Real);
+	Begin
+		fYmetre := aYMetre;
+		fYPixel	:= metreToPixel(aYMetre);
+	End;
+
+Function Cposition.GetXPixel() : Integer;
+	Begin
+		GetXPixel := fXPixel;
+	End;
+
+Function Cposition.GetYPixel() : Integer;
+	Begin
+		GetYPixel := fYPixel;
+	End;
+
+Function Cposition.GetXMetre() : Real;
+	Begin
+		GetXMetre := fXMetre;
+	End;
+
+Function Cposition.GetYMetre() : Real;
+	Begin
+		GetYMetre := fYmetre;
+	End;
+
+//Conversion Functions
+Function Cposition.pixelToMetre(aPos : Integer) : Real;
+	Begin
+		pixelToMetre := 42;
+	End;
+
+Function Cposition.metreToPixel(aPos : Real) : Integer;
+	Begin
+		metreToPixel := 42;
 	End;
 
 end.
