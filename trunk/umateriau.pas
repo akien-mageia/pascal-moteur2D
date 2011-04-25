f6c3e00213087a6c6a319d9a98df52dba509a68e
tpourcelot <tpourcelot@6f620d79-b0f1-4916-b3a6-c651a60d58ee>
2011-04-25 21:31:20 +0000

    Ajout de la doc latex + implémentation umateriau

--- trunk/umateriau.pas
+++ trunk/umateriau.pas
@@ -12,7 +12,52 @@ type CMateriau = Class
 		fIndex: integer;
 		fCoefChoc: real;
 	end;
-
+	
+	public
+		Constructor Create(aIndex : Integer,
+							aCoef : Real);
+		
+		Destructor	Destroy; override;
+		
+		//Accessors
+		function GetIndex : Integer;
+		function GetCoef  : Real;
+		procedure SetIndex( aIndex : Integer);
+		procedure SetCoef (	aCoef  : Real); 
+	
+	end;
 implementation
+
+Constructor CMateriau.Create(aIndex : Integer,
+							 aCoef	: Real);
+	Begin
+		SetIndex( aIndex );
+		SetCoef	(  aCoef );
+	End;
+
+Destructor CMateriau.Destroy;
+	Begin
+		Inherited;
+	End;
+
+Function CMateriau.GetIndex() : Integer;
+	Begin
+		GetIndex := fIndex;
+	End;
+
+Function CMateriau.GetCoef()  : Real;
+	Begin
+		GetCoef := fCoef;
+	End;
+	
+Procedure CMateriau.SetCoef( aCoef : Real);
+	Begin
+		fCoef := aCoef;
+	End;
+
+Procedure CMateriau.SetIndex( aIndex : Integer);
+	Begin
+		fIndex := aIndex;
+	End;
 
 end.
