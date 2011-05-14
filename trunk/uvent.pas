63f5f9c18db42772f7740f434cb02d66c8e2f0d2
rverscheld <rverscheld@6f620d79-b0f1-4916-b3a6-c651a60d58ee>
2011-05-14 19:52:10 +0000

    Copie de branches/clean_trunk dans trunk/ Manque les doc à rajouter à partir d'une ancienne révision.

---
+++ trunk/uvent.pas
@@ -0,0 +1,30 @@
+//***********************************************//
+//       Unit that deal with the wind		     //
+//                                               //
+//    TODO : implementation of CalculForce       //
+//***********************************************//
+
+unit UVent;
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, UTorseur, UForme, UVitesse;
+
+type CVent = Class(CTorseur)
+	public
+		Procedure calculForce(aForme: CForme ; aVitesse: CVitesse); override;
+
+	end;
+
+implementation
+
+Procedure CVent.calculForce(aForme: CForme ; aVitesse: CVitesse);
+	Begin
+
+
+	End;
+
+end.
+
